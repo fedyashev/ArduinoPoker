a73a5ec16c9a04aeaeab22ad4465031b95a557d6
Andrei Fedyashev <fedjashev@gmail.com>
2017-02-03 21:11:38 +0200

    Add glyphs. Start develop class Poker

--- Poker.ino
+++ Poker.ino
@@ -13,28 +13,100 @@
 namespace hw {
 
 // Display settings
-const int LCD_RS = 4;  // 8
-const int LCD_ENABLE = 5;  // 9
-const int LCD_D0 = 10;  // 4
-const int LCD_D1 = 11;  // 5
-const int LCD_D2 = 12;  // 6
-const int LCD_D3 = 13;  // 7
+const int LCD_RS = 8;  // 4
+const int LCD_ENABLE = 9;  // 5
+const int LCD_D0 = 4;  // 10
+const int LCD_D1 = 5;  // 11
+const int LCD_D2 = 6;  // 12
+const int LCD_D3 = 7;  // 13
 const int LCD_ROWS = 2;
 const int LCD_COLS = 16;
 
 // Keypad settings
 const int KEYPAD_PIN = 0;
 
+// Chars
+byte card_backside[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte heart[8] = {
+  B01010,
+  B11111,
+  B11111,
+  B11111,
+  B01110,
+  B00100,
+  B00000,
+  B00000,
+};
+
+byte diamond[8] = {
+  B00100,
+  B01110,
+  B11111,
+  B11111,
+  B01110,
+  B00100,
+  B00000,
+  B00000,
+};
+
+byte spade[8] = {
+  B00100,
+  B01110,
+  B11111,
+  B11111,
+  B00100,
+  B01110,
+  B00000,
+  B00000,
+};
+
+byte club[8] = {
+  B01110,
+  B01110,
+  B11111,
+  B11111,
+  B00100,
+  B01110,
+  B00000,
+  B00000,
+};
+
 class Display {
   public:
     Display() : lcd_(LCD_RS, LCD_ENABLE, LCD_D0, LCD_D1, LCD_D2, LCD_D3) {
       lcd_.begin(LCD_COLS, LCD_ROWS);
+      lcd_.createChar(0, card_backside);
+      lcd_.createChar(1, heart);
+      lcd_.createChar(2, diamond);
+      lcd_.createChar(3, spade);
+      lcd_.createChar(4, club);
     }
+
+    LiquidCrystal getLCD() { return lcd_; }
 
     template<typename T>
     void print(int row, int col, const T& val) {
       lcd_.setCursor(col, row);
       lcd_.print(val);
+    }
+
+    void setCursor(uint8_t row, uint8_t col) { lcd_.setCursor(col, row); }
+
+    void write(uint8_t glyph) { lcd_.write(glyph); }
+
+    void printGlyph(uint8_t row, uint8_t col, uint8_t glyph) {
+      lcd_.setCursor(col, row);
+      lcd_.write(glyph);
     }
     
     void clear() {
@@ -90,6 +162,79 @@ class Keypad {
 
 namespace Game {
 
+class Poker {
+
+  char card_rank[13] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+
+  struct Card {
+    char suit;
+    char rank;
+  };
+
+  struct Player {
+    Card hand[5] = { { '0', '0' },{ '0', '0' },{ '0', '0' },{ '0', '0' },{ '0', '0' } };
+    int coins = 0;
+  };
+
+  public:
+    Poker() : player_(), arduino_(), bank_coins_(0), start_game_(false) {}
+
+    void startGame() {
+      player_.coins = 500;
+      arduino_.coins = 500;
+      bank_coins_ = 0;
+      start_game_ = true;
+    }
+
+    void startTurn() {}
+    void dealCards() {}
+    void printPlayerHand() {
+      if (start_game_) {
+        for (uint8_t i = 0; i < 5; ++i) {
+          
+        }
+      } else {
+        hw::lcd.printGlyph(0, 0, 0);
+        hw::lcd.printGlyph(0, 1, 0);
+        hw::lcd.printGlyph(0, 2, 0);
+        hw::lcd.printGlyph(0, 3, 0);
+        hw::lcd.printGlyph(0, 4, 0);
+        hw::lcd.printGlyph(1, 0, 0);
+        hw::lcd.printGlyph(1, 1, 0);
+        hw::lcd.printGlyph(1, 2, 0);
+        hw::lcd.printGlyph(1, 3, 0);
+        hw::lcd.printGlyph(1, 4, 0);
+      }
+    }
+    
+    void printArduinoCloseHand() {
+      hw::lcd.printGlyph(0, 11, 0);
+      hw::lcd.printGlyph(0, 12, 0);
+      hw::lcd.printGlyph(0, 13, 0);
+      hw::lcd.printGlyph(0, 14, 0);
+      hw::lcd.printGlyph(0, 15, 0);
+      hw::lcd.printGlyph(1, 11, 0);
+      hw::lcd.printGlyph(1, 12, 0);
+      hw::lcd.printGlyph(1, 13, 0);
+      hw::lcd.printGlyph(1, 14, 0);
+      hw::lcd.printGlyph(1, 15, 0);
+    }
+
+    void printArduinoOpenHand() {}
+    void playerAdd() {}
+    void playerPass() {}
+    void playerOpen() {}
+    void arduinoAdd() {}
+    void arduinoPass() {}
+    void arduinoOpen() {}
+
+  private:
+    Player player_;
+    Player arduino_;
+    int bank_coins_;
+    bool start_game_;
+};
+
 class Screen {
   public:
     Screen() : prev_(0), next_(0) {}
@@ -144,14 +289,12 @@ class MainScreen : public Screen {
 
 class TableScreen : public Screen {
   public:
-    TableScreen() : Screen() {}
+    TableScreen(Poker& poker) : Screen(), poker_(poker) {}
     
     void show() override {
       hw::lcd.clear();
-      hw::lcd.print(0, 0, "* * *");
-      hw::lcd.print(1, 0, " * * ");
-      hw::lcd.print(0, 11, "* * *");
-      hw::lcd.print(1, 11, " * * ");
+      poker_.printPlayerHand();
+      poker_.printArduinoCloseHand();
       hw::lcd.print(0, 6, "BANK");
       hw::lcd.print(1, 6, "0$");
     }
@@ -159,7 +302,7 @@ class TableScreen : public Screen {
     void action(int button) {}
     
   private:
-  
+    Poker& poker_;
 };
 
 class MoneyScreen : public Screen {
@@ -167,6 +310,7 @@ class MoneyScreen : public Screen {
     MoneyScreen() : Screen() {}
     
     void show() override {
+      hw::lcd.clear();
       hw::lcd.print(0, 0, "Player");
       hw::lcd.print(1, 0, "0$");
       hw::lcd.print(0, 9, "Arduino");
@@ -184,6 +328,7 @@ class HelpScreen : public Screen {
     HelpScreen() : Screen() {}
     
     void show() override {
+      hw::lcd.clear();
       hw::lcd.print(0, 0, "S-open L-pass");
       hw::lcd.print(1, 0, "R-add U/D-scroll");
     }
@@ -196,9 +341,10 @@ class AuthorScreen : public Screen {
     AuthorScreen() : Screen() {}
     
     void show() override {
+      hw::lcd.clear();
       hw::lcd.print(0, 0, "Fedyashev");
       hw::lcd.print(1, 0, "Andrei");
-      hw::lcd.print(1, 10, "@2017");
+      hw::lcd.print(1, 12, "2017");
     }
     
     void action(int button) override {}
@@ -206,7 +352,7 @@ class AuthorScreen : public Screen {
 
 class Controller {
   public:
-    Controller() : current_(0) {createScreenStructure();}
+    Controller() : current_(0), poker_() {createScreenStructure();}
     ~Controller() {deleteScreenStructure();}
     
     void run() {
@@ -220,6 +366,11 @@ class Controller {
           current_ = current_->getNext();
           current_->show();
           break;
+        case hw::Keypad::BUTTON_SELECT:
+        case hw::Keypad::BUTTON_LEFT:
+        case hw::Keypad::BUTTON_RIGHT:
+          current_->action(button);
+          break;
       }
     }
     
@@ -228,7 +379,7 @@ class Controller {
   private:
     void createScreenStructure() {
       Screen* main_screen = new MainScreen();
-      Screen* tabel_screen = new TableScreen();
+      Screen* tabel_screen = new TableScreen(poker_);
       Screen* money_screen = new MoneyScreen();
       Screen* help_screen = new HelpScreen();
       Screen* author_screen = new AuthorScreen();
@@ -262,6 +413,7 @@ class Controller {
     }
   
     Screen* current_;
+    Poker poker_;
 };
 
 };  // namespace Game
